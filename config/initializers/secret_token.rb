14dcd3bed9901eef5a2a86e934fa171b6f7a3563
unknown <Nausheen@.(none)>
2011-09-04 01:27:37 +0500

    commit new rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventChampion::Application.config.secret_token = '0b73791d96ac6f4119ddec20e42287b745ed3825b2588e5c1331c1e521180a082c5b8ad25a9e0a7ca69f91d359d0691c0deaee4756848514ec976d89caa06f89'
